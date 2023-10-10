40030e0ccbbb927ee9e1cec6abb4a693b0877b91
Nisha <patar.nisha@gmail.com>
2023-10-10 23:38:08 +0530

    added questions via API (fetch)

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+export default function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("error"));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions</p>
+      </Main>
+    </div>
+  );
+}
