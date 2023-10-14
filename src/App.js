6c6f96ec0c9011255a1b30fb9cab05deecaa3dd5
Nisha <patar.nisha@gmail.com>
2023-10-14 22:47:21 +0530

    fetched question from fake API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import { useReducer } from "react";
+
+const initialState = {
+  questions: [],
+
+  //'loading, error, ready, acting, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("error"));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
 
   return (
